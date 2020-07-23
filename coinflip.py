f37fb547c22ddec396007dc2d2bfed73f6308df0
thedita <theditapedersen@gmail.com>
2020-07-23 00:45:25 -0400

    added 6-sided dice roll and the option to play either game

--- coinflip.py
+++ coinflip.py
@@ -1,17 +1,22 @@
 import random
+print 'Welcome to the Game of Chance!'
+print 'Which game would you like to play?'
+
 correct_guesses = 0
 total_guesses = 0
 
-print 'Welcome to the Coin Flip Game!'
 def make_guess():
     print "Correct Guesses:", correct_guesses
     print "Total Guesses:", total_guesses
     guess = raw_input('Guess either Heads or Tails\n')
     return guess
-guess1 = make_guess()
 
-raw_input('Press enter to flip the coin')
-
+def make_dice_guess():
+    print "Correct Guesses:", correct_guesses
+    print "Total Guesses:", total_guesses
+    guess = raw_input('Guess a number from 1-6\n')
+    return guess
+    
 def print_result(str):
     print 'You got %s!' % str
     
@@ -20,8 +25,7 @@ def quit_or_again():
     if answer == 'quit' or answer == 'Quit':
         quit()
     else:
-        guess = make_guess()
-        flip_coin(guess)
+        which_game()
     
 def flip_coin(guess):
     flip = random.choice([1, 2])
@@ -40,5 +44,26 @@ def flip_coin(guess):
         print 'You guessed incorrectly!'
     quit_or_again()
 
-flip_coin(guess1)
+def roll_dice(guess):
+    roll = random.choice([1, 2, 3, 4, 5, 6])
+    print 'You rolled a %d!' % roll
+    global total_guesses
+    total_guesses += 1
+    if roll == guess:
+        global correct_guesses
+        correct_guesses += 1
+        print 'You guessed correctly!'
+    elif roll != guess:
+        print 'You guessed incorrectly!'
+    quit_or_again()
+
+def which_game():
+    game = int(raw_input('Enter 1 for Coin Flip and 2 for 6-sided dice\n'))
+    if game == 1:
+        coin_guess = make_guess()
+        flip_coin(coin_guess)
+    elif game == 2:
+        dice_guess = make_dice_guess()
+        roll_dice(dice_guess)
+which_game()
 
